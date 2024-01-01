c3f326e9903a62e54691c807b233988d87dc2eac
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2024-01-01 17:31:21 +0100

    2024

--- src/main/java/de/florianmichael/baseproject/Example.java
+++ src/main/java/de/florianmichael/baseproject/Example.java
@@ -1,12 +1,12 @@
 /*
  * This file is part of BaseProject - https://github.com/FlorianMichael/BaseProject
- * Copyright (C) 2023 FlorianMichael/EnZaXD and contributors
+ * Copyright (C) 2024 FlorianMichael/EnZaXD <florian.michael07@gmail.com> and contributors
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -22,4 +22,5 @@ public class Example {
     public static void main(String[] args) {
         System.out.println("Welcome to Java!");
     }
+
 }
