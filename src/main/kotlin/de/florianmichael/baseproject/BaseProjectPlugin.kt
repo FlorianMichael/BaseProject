1b4de7361353e41c42d4acc9ed7ea3606fc5a031
FlorianMichael <florian.michael07@gmail.com>
2025-12-12 15:23:46 +0100

    Cleanup code

--- src/main/kotlin/de/florianmichael/baseproject/BaseProjectPlugin.kt
+++ src/main/kotlin/de/florianmichael/baseproject/BaseProjectPlugin.kt
@@ -23,7 +23,7 @@ import org.gradle.api.Project
 open class BaseProjectPlugin : Plugin<Project> {
 
     override fun apply(target: Project) {
-        target.logger.lifecycle("BaseProject: " + BaseProjectPlugin::class.java.getPackage().implementationVersion)
+        target.logger.lifecycle("BaseProject: " + javaClass.getPackage().implementationVersion)
     }
 
 }
